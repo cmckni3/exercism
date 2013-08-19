59434e5d2647f497aa9e8c974ff0155c8916b7fa
Chris McKnight <cmckni3@gmail.com>
2013-08-19 09:49:41 -0500

    Use slice

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -2,7 +2,7 @@ module.exports = function()
 {
   this.hey = function(message)
   {
-    var question = (message[message.length-1] === '?');
+    var question = (message.slice(message.length-1) === '?');
     var forceful = (message.toUpperCase() === message);
     if (message === '') return 'Fine. Be that way!';
     else if (forceful) return 'Woah, chill out!';
